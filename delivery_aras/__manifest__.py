c8d9147128655c95649b3fc3cedc5348f0908f80
Ismail Cagan Yilmaz <ismail.cagan.yilmaz@gmail.com>
2024-12-28 16:19:22 +0300

    [MIG] delivery_aras: Migration to 16.0 #78

---
+++ delivery_aras/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2024 Ahmet Yiğit Budak (https://github.com/yibudak)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# Copyright 2024 Ismail Cagan Yilmaz (https://github.com/milleniumkid)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Delivery Aras",
+    "summary": "Delivery Carrier implementation for Aras Kargo API",
+    "version": "16.0.1.1.0",
+    "category": "Stock",
+    "website": "https://github.com/odoo-turkey",
+    "author": "Yiğit Budak, Odoo Turkey Localization Group",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["delivery_integration_base"],
+    "external_dependencies": {"python": ["phonenumbers", "zeep"]},
+    "data": [
+        "views/delivery_aras_view.xml",
+        'report/aras_carrier_label.xml',
+        'report/aras_sms_template.xml',
+        # 'report/reports.xml',
+
+    ],
+}
